78fdd43d453d639b776a06093dcf90e8abff048b
Ryan C. Durham <ryan@stagerightlabs.com>
2015-11-30 15:07:50 -0800

    Add bind method to laravel4 module

--- src/Modules/SRLabsLaravel4.php
+++ src/Modules/SRLabsLaravel4.php
@@ -4,11 +4,21 @@ namespace Codeception\Module;
 class SRLabsLaravel4 extends \Codeception\Module\Laravel4
 {
     /**
-     * Provides access the Laravel application object.
+     * Allow the Codeception Actor to add a binding to the Laravel IOC
      *
      * @return \Illuminate\Foundation\Application
      */
-    public function bindIntance($abstract, $instance)
+    public function bind($abstract, $instance, $shared = false)
+    {
+        $this->app->bind($abstract, $instance, $shared = false);
+    }
+
+    /**
+     * Allow the Codeception Actor to bind an instantiated object to the Laravel IOC
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function bindInstance($abstract, $instance)
     {
         $this->app->instance($abstract, $instance);
     }
