17c24f682ae400c0c8f35d81fbe30753c80d040c
Ryan C. Durham <ryan@stagerightlabs.com>
2015-11-30 15:55:19 -0800

    Add custom codeception l4 connector

--- src/Modules/SRLabsLaravel4.php
+++ src/Modules/SRLabsLaravel4.php
@@ -1,14 +1,27 @@
 <?php
 namespace Codeception\Module;
 
+use SRLabs\Utilities\Libraries\Laravel4Connector;
+
 class SRLabsLaravel4 extends \Codeception\Module\Laravel4
 {
+    /**
+     * Initialize hook.
+     */
+    public function _initialize()
+    {
+        $this->checkStartFileExists();
+        $this->registerAutoloaders();
+        $this->revertErrorHandler();
+        $this->client = new Laravel4Connector($this);
+    }
+
     /**
      * Allow the Codeception Actor to add a binding to the Laravel IOC
      *
      * @return \Illuminate\Foundation\Application
      */
-    public function bind($abstract, $instance, $shared = false)
+    public function bindService($abstract, $instance, $shared = false)
     {
         $this->app->bind($abstract, $instance, $shared = false);
     }
